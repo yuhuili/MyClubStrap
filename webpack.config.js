f43ad28142db4130bb77280a41bfd16b8514086b
GabrielAlacchi <alacchi.g@gmail.com>
2016-09-17 01:41:48 -0400

    Made it easier to add entry points to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,17 @@
 var path = require('path');
 var webpack = require('webpack');
 
+// To add a new entry point just add another to this list
+var entryPoints = ['./web/src/main.js'];
+
 function getConfig(debug) {
   return {
     context: __dirname,
-    entry: './web/src/main.js',
+    entry: entryPoints,
     devtool: debug ? 'inline-source-map' : null,
     output: {
       path: path.join(__dirname, 'build/src'),
-      filename: 'bundle.js'
+      filename: '[name].js'
     },
     plugins: debug ? null : [
       new webpack.optimize.UglifyJsPlugin({minimize: true})
