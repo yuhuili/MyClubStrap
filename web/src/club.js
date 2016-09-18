7b6d0b4cc29d1cce1ae41dedb8572451877b0c99
GabrielAlacchi <alacchi.g@gmail.com>
2016-09-17 22:31:00 -0400

    Began work on club joining features

--- web/src/club.js
+++ web/src/club.js
@@ -75,3 +75,36 @@ function parse(val) {
     });
   return result;
 }
+
+var firebaseUser = null;
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user && user.emailVerified) {
+
+    firebaseUser = user;
+
+    // Render User in the navbar
+    // Fetch the user's profile information once
+    firebase.database().ref('/users/' + user.uid + '/public').once('value')
+      .then(function(snapshot) {
+        // Set the profile link to "Welcome " + firstName
+        $('#profile-link').children('span').html('Welcome ' + snapshot.val().firstName);
+
+        $('#profile-link').parents().removeClass('hidden');
+        $('#sign-out').parents().removeClass('hidden');
+        $('#sign-in-reg').addClass('hidden');
+      });
+  } else {
+    $('#profile-link').addClass('hidden');
+    $('#sign-out').addClass('hidden');
+    $('#sign-in-reg').removeClass('hidden');
+  }
+});
+
+function joinClub() {
+  if (firebaseUser) {
+
+  } else {
+
+  }
+}
